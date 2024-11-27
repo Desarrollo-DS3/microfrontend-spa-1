1e055a35dc600fc27f234519e8f839bef06ed0a2
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-11-27 14:44:07 -0500

    feat: implement RegisterForm.jsx on mf-register and load fonts

---
+++ mf-register/src/root.component.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import RegisterForm from "./components/RegisterForm";
+
+export default function Root(props) {
+  return (
+    <div className="root-form-container">
+      <RegisterForm />
+    </div>
+  );
+}
