48dcc3fde0def4d52be0fda6841b41e3b6960198
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-11-26 21:39:14 -0500

    feat: implement header with styles and functionality

--- mf-header/src/root.component.jsx
+++ mf-header/src/root.component.jsx
@@ -1,11 +1,27 @@
 import "./styles.css";
 
 export default function Root(props) {
+  const navigateToNews = () => {
+    window.location.href = "/news"; // Cambia la URL a /news
+  };
+
+  const navigateToRegister = () => {
+    window.location.href = "/register"; // Cambia la URL a /register
+  };
+
   return (
     <header>
-      <nav>
-        <h1>Mi Aplicación - Home</h1>
-      </nav>
+      <div className="top-bar">
+        <h1>Mi Sitio Web</h1>
+      </div>
+      <div className="bottom-bar">
+        <button className="nav-button" onClick={navigateToNews}>
+          Noticias
+        </button>
+        <button className="nav-button" onClick={navigateToRegister}>
+          Registro
+        </button>
+      </div>
     </header>
   );
 }
