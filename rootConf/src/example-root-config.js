7b77a3de41de5e43efaab128dc9b5b4a4faf3649
JuanLoaiza007 <juandaloaiza723@gmail.com>
2024-11-26 20:16:25 -0500

    fix: set ErrosUI to load some when a mf fails

--- rootConf/src/example-root-config.js
+++ rootConf/src/example-root-config.js
@@ -6,15 +6,36 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+// Datos de error UI
+const layoutData = {
+  errors: {
+    headerError: "<h1>Error en la aplicación Header</h1>",
+    newsError: "<h1>Error en la aplicación News</h1>",
+  },
+};
+
+const routes = constructRoutes(microfrontendLayout, layoutData);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
-    return import(/* webpackIgnore: true */ name);
+    return import(/* webpackIgnore: true */ name).catch((error) => {
+      console.error(`Error loading ${name}:`, error);
+      // throw new Error(`Application ${name} failed to load`);
+    });
   },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
 
-applications.forEach(registerApplication);
+applications.forEach((app) => {
+  try {
+    registerApplication(app);
+  } catch (error) {
+    console.error(`Failed to register application: ${app.name}`, error);
+  }
+});
+
+const layoutEngine = constructLayoutEngine({ routes, applications });
 layoutEngine.activate();
+
+// Iniciar single-spa
 start();
